eb92cb95c9a1a6939fc33445d0ac9318a5b46428
clintont <clintont@sfu.ca>
2017-07-21 13:38:54 -0700

    9state with random A B matrx

--- hmmtest.R
+++ hmmtest.R
@@ -8,6 +8,8 @@ test <- read.csv("test_v1.csv")
 # Extract the global_active_power column
 Gap.col <- data.frame(X = train$Global_active_power[1:10000])
 #Tgap.col <- data.frame(test$Global_active_power[1:20000])
+# trainGap <- formatMhsmm(data.frame(X = train$Global_active_power[1:10000]))
+# testGap <- formatMhsmm(data.frame(test$Global_active_power[764437:774437]))
 
 # Replace 0 with NA
 Gap.col[is.na(Gap.col)] <- 0
@@ -27,29 +29,28 @@ library(mhsmm)
 J <- 9
 
 # Set init value pi
-#pi <- rep(1/J, J)
-pi <- c(0.111,0.050,0.081
-        ,0.090,0.075,0.035
-        ,0.250,0.170,0.138)
+# #pi <- rep(1/J, J)
+# pi <- c(0.111,0.050,0.081
+#         ,0.090,0.075,0.035
+#         ,0.250,0.170,0.138)
+pi <- c(runif(J, 0, 1))
 
 # Build transition matrix A, entry by row
-A <- matrix(c(0.8, 0.5, 0.1, 0.05, 0.2, 0.5, 0.15, 0.3, 0.4,
-              0.8, 0.5, 0.1, 0.05, 0.2, 0.5, 0.15, 0.4, 0.3,
-              0.8, 0.5, 0.1, 0.05, 0.4, 0.5, 0.15, 0.3, 0.2,
-              0.4, 0.5, 0.1, 0.05, 0.2, 0.5, 0.15, 0.3, 0.8,
-              0.8, 0.1, 0.5, 0.05, 0.2, 0.5, 0.15, 0.3, 0.4,
-              0.8, 0.5, 0.1, 0.05, 0.2, 0.15, 0.5, 0.3, 0.4,
-              0.15, 0.5, 0.1, 0.05, 0.2, 0.5, 0.8, 0.3, 0.4,
-              0.8, 0.05, 0.1, 0.5, 0.2, 0.5, 0.15, 0.3, 0.4,
-              0.8, 0.5, 0.1, 0.05, 0.5, 0.2, 0.15, 0.3, 0.4), nrow = J)
+# A <- matrix(c(0.8, 0.5, 0.1, 0.05, 0.2, 0.5, 0.15, 0.3, 0.4,
+#               0.8, 0.5, 0.1, 0.05, 0.2, 0.5, 0.15, 0.4, 0.3,
+#               0.8, 0.5, 0.1, 0.05, 0.4, 0.5, 0.15, 0.3, 0.2,
+#               0.4, 0.5, 0.1, 0.05, 0.2, 0.5, 0.15, 0.3, 0.8,
+#               0.8, 0.1, 0.5, 0.05, 0.2, 0.5, 0.15, 0.3, 0.4,
+#               0.8, 0.5, 0.1, 0.05, 0.2, 0.15, 0.5, 0.3, 0.4,
+#               0.15, 0.5, 0.1, 0.05, 0.2, 0.5, 0.8, 0.3, 0.4,
+#               0.8, 0.05, 0.1, 0.5, 0.2, 0.5, 0.15, 0.3, 0.4,
+#               0.8, 0.5, 0.1, 0.05, 0.5, 0.2, 0.15, 0.3, 0.4), nrow = J)
+
+A <- matrix(c(runif(J*J, 0,1)))
 
 # Build emission matrix
-B <- list(mu = c(0.16, 0.23, 0.283,
-                 0.329, 0.391, 0.457,
-                 0.51, 0.539, 0.57), 
-          sigma = c(0.043, 0.011, 0.017, 
-                    0.013, 0.017, 0.021,
-                    0.01, 0.008, 0.01))
+B <- list(mu = c(runif(J, 0,1.5)), 
+          sigma = c(runif(J, 0,0.5)))
 
 # Build hmm model
 model <- hmmspec(init = pi, trans = A, parms.emis = B, dens.emis = dnorm.hsmm, mstep = 10)
@@ -68,8 +69,8 @@ plot(yhat)
 
 
 # Test hmm model
-yhat1 <- predict(hmm, trainGap$x)
-yhat2 <- predict(hmm, testGap$x)
+# yhat1 <- predict(hmm, trainGap$x)
+# yhat2 <- predict(hmm, testGap$x)
 # J <- 3
 # init <- c(0,0,1)
 # P <- matrix(c(0,.1,.4,.5,0,.6,.5,.9,0),nrow=J)
